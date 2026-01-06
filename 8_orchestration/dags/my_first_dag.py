53f7ae507b2ed7fb721a3a90594233eb4c8a8f3d
Manzo <3mg.manzone@gmail.com>
2026-01-06 17:08:35 +0000

    esercizio

--- 8_orchestration/dags/my_first_dag.py
+++ 8_orchestration/dags/my_first_dag.py
@@ -24,13 +24,28 @@ with DAG(
     
     load_dataset_task = load_dataset()
 
-    @task()
+    @task.branch()
+    def check_path(dataset_path):
+        if dataset_path:
+            return "training_ML_model"
+        else:
+            return "raise_error_dataset_path"
+
+    check_path_task = check_path(load_dataset_task)
+
+    @task(task_id = "training_ML_model")
     def training_ML_model(dataset_path):
         print(f"read dataset from {dataset_path}")
         print("dataset letto")
         print("modello addestrato")
         return "ML_flow_model_id"
 
+    @task(task_id = "raise_error_dataset_path")
+    def raise_error_dataset_path():
+        raise Exception("Percorso del dataset nullo")
+
+    raise_error_dataset_path_task = raise_error_dataset_path()
+
     training_ML_model_task = training_ML_model(load_dataset_task)
 
-    inizio >> load_dataset_task >> training_ML_model_task
+    inizio >> load_dataset_task >> check_path_task >> [ training_ML_model_task, raise_error_dataset_path_task ]
